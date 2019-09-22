e17cd4260954970c17ba34cfe0bec2deb036fa36
hexaviator <hexaviatordev@gmail.com>
2019-09-22 20:53:45 +0300

    adding copy private key

--- src/components/PrivateKeyModal.vue
+++ src/components/PrivateKeyModal.vue
@@ -4,7 +4,8 @@
         <div v-if="isOpen" class="container">
             <div class="row">
                 <div class="col-12">
-                    Private Key : {{ this.wallet.prvk}}
+                        Private Key : {{ this.wallet.prvk}}
+                    <b-button @click="copyText" variant="outline-success" class="btn">Copy to clipboard</b-button>
                 </div>
             </div>
         </div>
@@ -27,6 +28,15 @@ export default {
     methods: {
         toggleModal() {
             this.isOpen = !this.isOpen
+        },
+        copyText() {
+            var dummy = document.createElement('textarea')
+            document.body.appendChild(dummy)
+            dummy.value = this.wallet.prvk
+            dummy.select()
+            document.execCommand('copy')
+            document.body.removeChild(dummy)
+            alert('private key copied to clipboard') // TODO make a modal
         }
     }
 }
@@ -39,4 +49,15 @@ export default {
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
+
+.btn {
+   position: relative;
+    margin-left: 30px;
+    height: 30px;
+    width: 120px;
+    font-size: 10px;
+    margin-top: 10px;
+    margin-bottom: 10px; 
+}
+
 </style>
